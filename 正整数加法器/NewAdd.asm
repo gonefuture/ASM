584a551a1487cb53f8bcb95aae5e25ba1c40e453
gonefuture <gone1135816685>
2016-11-27 00:00:05 +0800

    或许要用CMP比较余数。

--- 正整数加法器/NewAdd.asm
+++ 正整数加法器/NewAdd.asm
@@ -17,7 +17,7 @@ START:
     MOV AX,DATAS
     MOV DS,AX
     LEA DX, BUFFSIZE
-    MOV AH, 0AH
+    MOV AH, 0AH     ;回车换行
     INT 21H
     MOV DL, 13
     MOV AH,2
@@ -33,39 +33,36 @@ START:
     MOV DH, 10
 T1:
 	MOV DL, 2BH
-	CMP DL, [BX]
+	CMP DL, [BX]  ;判断是否是加号
 	ADD BX, 1
-	JZ T2
-	MOV AL, [BX]
-	SUB AL, 30H
+	JZ T2         ;发现加号则跳到T2                
+	MOV AL, [BX]  
+	SUB AL, 30H    ;字符变数组
 	MUL DH
-	MOV OP1,AX
+	MOV OP1,AX   ;放入OP1
 	MOV AH, 0
   	LOOP T1
   	
   	
-T2:
-	MOV AL, [BX]
+T2:  
+	MOV AL, [BX]  ;取加号后的加数
 	SUB AL, 30H
 	MUL DH
-	MOV OP1,AX
+	MOV OP1,AX  ;放入OP2
 	MOV AH, 0
 	LOOP T2
     
     
-    MOV AX, OP1
-    ADD AX, OP2
+    MOV AX, OP1  ;两数相加
+    ADD AX, OP2     
     LEA BX, RESULT
-    
-    MOV DL, AL
-    ADD DL, 30H
-    MOV AH,2
-    INT 21H
-    
-    MOV CX,3
+
+    MOV CL,ACT
+    MOV CH,0
+   
    
 L1:
-	MOV DL, 10
+	MOV DL, 10   ;除10取出
 	DIV DL
 	MOV [BX],AH
 	MOV AH,0
@@ -73,11 +70,12 @@ L1:
 	
 LOOP L1	
 	
-	LEA BX, RESULT
+	LEA BX, RESULT   ;
 	ADD BX, 2
-	MOV CX, 3
+	MOV CL,ACT
+    MOV CH,0
 L2:		
-	MOV DL,[BX]
+	MOV DL,[BX]  ;显示数字
 	ADD DL,30H
 	MOV AH, 2
     INT 21H
@@ -90,6 +88,7 @@ LOOP L2
     INT 21H
 CODES ENDS
     END START
+
 
 
 
